0b421bfd656b1b4ab3d4bab70574fbd6cef3a8b8
Augusto Aguirre <lugemex@gmail.com>
2020-06-27 00:17:15 -0500

    Agregar archivos de proyecto.

---
+++ medicamentosWPF/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace medicamentosWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxCantidad_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void buttonBorrar_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxMedicamento.Clear();
+            comboBoxTipo.SelectedIndex = -1;
+            textBoxCantidad.Clear();
+            radioButtonCemefar.IsChecked = false;
+            radioButtonCofarma.IsChecked = false;
+            radioButtonEmpsephar.IsChecked = false;
+            checkBoxPrimaria.IsChecked = false;
+            checkBoxSecudnaria.IsChecked = false;           
+        }
+    }
+}
