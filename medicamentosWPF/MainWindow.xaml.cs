0f64f3b8fa528dcee0fd682331fd370c02ce4d0e
Augusto Aguirre <lugemex@gmail.com>
2020-07-01 13:49:25 -0500

    opción de envío a dos suscursales

--- medicamentosWPF/MainWindow.xaml.cs
+++ medicamentosWPF/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace medicamentosWPF
 {
@@ -37,9 +25,9 @@ namespace medicamentosWPF
             radioButtonCofarma.IsChecked = false;
             radioButtonEmpsephar.IsChecked = false;
             checkBoxPrimaria.IsChecked = false;
-            checkBoxSecundaria.IsChecked = false;           
+            checkBoxSecundaria.IsChecked = false;
         }
-     
+
         private void ButtonConfirmar_Click(object sender, RoutedEventArgs e)
         {
             //comprobación del nombre de medicamento
@@ -55,14 +43,14 @@ namespace medicamentosWPF
             //comprobación de una cantidad positiva
             else if (string.IsNullOrEmpty(TextBoxCantidad.Text))
             {
-                MessageBox.Show("Favor de escribi la cantidad del medicamento");
+                MessageBox.Show("Favor de escribir la cantidad del medicamento");
             }
             else if (Convert.ToDecimal(TextBoxCantidad.Text) <= 0)
             {
                 MessageBox.Show("Favor de seleccionar una cantidad positiva");
             }
             //comprobación de distribuidor
-            else if ((bool)(!radioButtonCemefar.IsChecked & !radioButtonCofarma.IsChecked & !radioButtonEmpsephar.IsChecked)==true)
+            else if ((bool)(!radioButtonCemefar.IsChecked & !radioButtonCofarma.IsChecked & !radioButtonEmpsephar.IsChecked) == true)
             {
                 MessageBox.Show("Favor de seleccionar algún distribuidor");
             }
@@ -92,43 +80,47 @@ namespace medicamentosWPF
                     Distribuidor = "Empsepar";
                 }
                 objWindowConfirmar.Title = Distribuidor;
-                //elegir dirección de envío
-                if ((bool)checkBoxPrimaria.IsChecked)
-                {
-                    Sucursal = "Calle de la Rosa n. 28.";
-                }
-                else if ((bool)checkBoxSecundaria.IsChecked)
-                {
-                    Sucursal = "Calle Alcazabilla n. 3.";
-                }
+                
                 objWindowConfirmar.TextBoxResumen.Text = TextBoxCantidad.Text + " unidades del " + comboBoxTipo.Text + " " + TextBoxMedicamento.Text;
-                objWindowConfirmar.TextBoxDireccion.Text = Sucursal;
+                //elegir dirección de envío
+                if ((bool)checkBoxPrimaria.IsChecked & !(bool)checkBoxSecundaria.IsChecked)
+                {
+                    objWindowConfirmar.TextBoxDireccion.Text = "Calle de la Rosa n. 28.";
+                }
+                if (!(bool)checkBoxPrimaria.IsChecked & (bool)checkBoxSecundaria.IsChecked)
+                {
+                    objWindowConfirmar.TextBoxDireccion.Text = "Calle Alcazabilla n. 3.";
+                }
+                if ((bool)checkBoxSecundaria.IsChecked & (bool)checkBoxPrimaria.IsChecked)
+                {
+                    objWindowConfirmar.TextBoxDireccion.Text = "Calle de la Rosa n. 28."+"\n"+ "Calle Alcazabilla n. 3.";
+                }
             }
 
-            
-            
+
+
         }
 
         private void CheckBoxPrimaria_Checked(object sender, RoutedEventArgs e)
         {
-            checkBoxSecundaria.IsChecked = false;
+//            checkBoxSecundaria.IsChecked = false;
         }
 
         private void CheckBoxSecundaria_Checked(object sender, RoutedEventArgs e)
         {
-            checkBoxPrimaria.IsChecked = false;
+  //          checkBoxPrimaria.IsChecked = false;
         }
 
         private void ComboBoxTipo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-                
+
         }
 
         private void TextBoxMedicamento_KeyDown(object sender, KeyEventArgs e)
         {
             // el evento e-Key devuelve el código de la tecla física (NO DEL ASCII)!! D0=34 y D9=43;A=44-Z=69;num 74-83
             //MessageBox.Show(Convert.ToString(e.Key));
-            if (e.Key==Key.Enter | e.Key == Key.Escape | e.Key == Key.RightShift | e.Key == Key.LeftShift | e.Key == Key.Capital| e.Key == Key.Oem3) //key.Oem3== ñ (146)
+            if (e.Key == Key.Enter | e.Key == Key.Escape | e.Key == Key.RightShift | e.Key == Key.LeftShift | e.Key == Key.Capital | e.Key == Key.Oem3) //key.Oem3== ñ (146)
             {
                 return;
             }
@@ -152,7 +144,7 @@ namespace medicamentosWPF
         {
             // el evento e-Key devuelve el código de la tecla física (NO DEL ASCII)!! D0=34 y D9=43;74-83
             //MessageBox.Show(Convert.ToString((int)e.Key));
-            if (e.Key == Key.Enter | e.Key==Key.Escape)
+            if (e.Key == Key.Enter | e.Key == Key.Escape)
             {
                 //MessageBox.Show("tecla válida");
                 return;
@@ -162,10 +154,10 @@ namespace medicamentosWPF
                 if (e.Key < Key.D0 | e.Key > Key.D9)// entre 34 y 43
                 {
                     MessageBox.Show("Favor de introducir sólo caracteres numéricos");
-                    e.Handled=true;  
+                    e.Handled = true;
                 }
             }
-            
+
             else if (e.Key > Key.NumPad9) //> 83
             {
                 MessageBox.Show("Favor de introducir sólo caracteres numéricos 2");
